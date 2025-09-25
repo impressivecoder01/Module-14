56a5be497fb5a49015167783210b7ac178937ee8
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-25 22:41:13 +0600

    class1

--- class1.js
+++ class1.js
@@ -1,1 +1,12 @@
 // compare variables and comparison operator
+// comparison
+// 1. bigger: >
+// 2. less:<
+// 3. equal: ==
+// 4. greater than or equal: >=
+// 5. less than or equal: <=
+// 6. not equal: !=
+
+// -----------------
+// 7. and: &&
+// 8. or : ||
