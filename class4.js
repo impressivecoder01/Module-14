7e373756e4b49a24e8ed3b13407e91541b64a3f8
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-27 00:07:50 +0600

    class4

---
+++ class4.js
@@ -0,0 +1,29 @@
+// Conditional branching if else condition
+// var age = 55;
+// if(age > -100){
+//     console.log('buy some food')
+// }
+// else{
+//     console.log("don't waste any money" )
+// }
+// if(condition){
+//     execute code if condition above is ture
+// }
+// else{
+//     execute code if the condition is false
+// }
+var weight = 20;
+if(weight > 10){
+    console.log('i can carry')
+}
+else{
+    console.log('i need help')
+}
+
+var price = 444;
+if(price <500){
+    console.log('i will eat some')
+}
+else(
+    console.log('do not have enough money')
+)
