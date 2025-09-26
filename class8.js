627efc77067c12e2e840715a6bfd70e78de37190
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-27 02:18:21 +0600

    class8

---
+++ class8.js
@@ -0,0 +1,39 @@
+//if-else shorthand ternary operator
+
+// ternary --> three parts
+// condition ? do something when true : do something when false
+
+const age = 20;
+// if (age >= 20){
+//     console.log('you can vote')
+// }
+// else{
+//     console.log('no vote')
+// }
+
+// age >= 21 ? console.log('can vote ') : console.log('can not vote')
+
+let price = 500;
+const isLeader = true;
+// if(isLeader == true){
+//     price= 0;
+// }
+// else{
+//     price = price + 132;
+// }
+// console.log(price)
+
+// price = isLeader == true ? 0 : price + 100;
+// if(isLeader == true){
+//     if(price >= 500){
+//         price /= 2;
+//     }
+// }
+// else{
+//         price += 100
+//     }
+//     console.log(price)
+
+// price = isLeader === true
+
+// price = isLeader == true ? price >= 500 ? price/=2 : 0 : price += 100;
