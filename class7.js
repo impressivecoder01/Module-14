dc8d115038bbe2a3afa079882971e6b6048d66d2
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-27 01:45:46 +0600

    class7

---
+++ class7.js
@@ -0,0 +1,13 @@
+// nested if-else condition
+const money = 3;
+if(money > 300){
+    console.log('rich')
+}
+else{
+    if(money >= 100){
+        console.log('earning money')
+    }
+    else{
+        console.log('done')
+    }
+}
