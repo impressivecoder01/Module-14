b5cc4bf5efee77d6ba8eb91dfa345667fb88cc19
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-28 01:03:27 +0600

    class9

---
+++ class9.js
@@ -0,0 +1,16 @@
+//logical not operator
+const isLeader = true;
+if(!isLeader){
+    console.log('take money')
+}
+else{
+    console.log('give money')
+}
+
+const pass = true;
+if(!pass){
+    console.log('done')
+}
+else{
+    console.log('not yet')
+}
